3811e024b8b00848737c5787b3ec8a689817d252
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2020-12-21 13:14:48 +0000

    Refactored code in CameraPage index

--- project/arec/src/views/CameraPage/index.vue
+++ project/arec/src/views/CameraPage/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="camera">
-    <h1>Hello World!</h1>
     <button @click="$router.go(-1)" >Go back!</button>
     <video id="webCam" autoplay="true"/>
   </div>
@@ -14,33 +13,34 @@ export default {
   name: "index",
   components: {},
   mounted() {
-    this.$nextTick(function () {
-      let video = document.querySelector('#webCam')
-
-      console.log("Fired in streamVide()")
-      if (navigator.mediaDevices.getUserMedia()) {
-        navigator.mediaDevices.getUserMedia({video: true, audio: false})
-            .then(function (stream) {
-              video.srcObject = stream;
-              this.$emit("video-stream", video);
-            }).catch(err => console.log(err));
-      }
-    })
+    // this.$nextTick(function () {
+    //   let video = document.querySelector('#webCam')
+    //
+    //   console.log("Fired in streamVide()")
+    //   if (navigator.mediaDevices.getUserMedia()) {
+    //     navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    //         .then(function (stream) {
+    //           video.srcObject = stream;
+    //           this.$emit("video-stream", video);
+    //         }).catch(err => console.log(err));
+    //   }
+    // })
+    this.renderCam(document.getElementById('webCam')); //run 'renderCam' when the Vue page loads
   },
+  methods: {
+    renderCam(video)
+    {
+      if (navigator.mediaDevices.getUserMedia()) {
+        navigator.mediaDevices.getUserMedia({video: true, audio: false}) //Ask for video permission
+            .then(function (stream) { //If video permission granted create a stream and render video
+              video.srcObject = stream;
+              this.$emit("video-stream", video);  //Send frames out of vue?
+            }).catch(err => console.log(err)); //If permissions are not granted or webcam doesn't work throw error
+      }
+    }
+    },
 
 }
-// streamVideo() {
-//   let video = document.querySelector('#webCam')
-//
-//   console.log("Fired in streamVide()")
-//   if (navigator.mediaDevices.getUserMedia()) {
-//     navigator.mediaDevices.getUserMedia({video: true})
-//         .then(function (stream) {
-//           video.srcObject = stream;
-//           this.$emit("video-stream", video);
-//         }).catch(err => console.log(err));
-//   }
-// }
 </script>
 
 <style scoped>
